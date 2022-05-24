bc862b25e22498a7251db9a75d5d571b0eec476a
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2022-05-24 00:16:36 -0300

    Adjusting some endpoints and services URLs to use a Kubernetes cluster

--- mini-microservice-boilerplate/blog/event-bus/index.js
+++ mini-microservice-boilerplate/blog/event-bus/index.js
@@ -12,19 +12,24 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event).catch((err) => {
+  axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('http://localhost:4001/events', event).catch((err) => {
+  axios
+    .post('http://comments-clusterip-srv:4001/events', event)
+    .catch((err) => {
+      console.log(err.message);
+    });
+  axios.post('http://query-clusterip-srv:4002/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('http://localhost:4002/events', event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post('http://localhost:4003/events', event).catch((err) => {
-    console.log(err.message);
-  });
+  axios
+    .post('http://moderation-clusterip-srv:4003/events', event)
+    .catch((err) => {
+      console.log(err.message);
+    });
 
+  console.log(`Event ${event.type} received`);
   res.send({ status: 'OK' });
 });
 
--- mini-microservice-boilerplate/blog/moderation/index.js
+++ mini-microservice-boilerplate/blog/moderation/index.js
@@ -8,10 +8,12 @@ app.use(bodyParser.json());
 app.post('/events', async (req, res) => {
   const { type, data } = req.body;
 
+  console.log('Event received: ', req.body.type);
+
   if (type === 'CommentCreated') {
     const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentModerated',
       data: {
         id: data.id,

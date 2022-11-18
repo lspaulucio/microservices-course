b14df5074dc9be8d66c502ff2bfbaa7c83bea1ea
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2022-11-17 23:19:23 -0300

    Adding tests

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,21 +1,22 @@
-import express from 'express';
-import { json } from 'body-parser';
+import mongoose from "mongoose";
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
+import { app } from "./app";
 
-const app = express();
-app.use(json());
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-app.use(errorHandler);
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {});
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!');
-});
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
